08db08238f41eff6cdca988f3c754de145cfcaa5
Arsna <arsna.business@protonmail.com>
2025-02-26 18:00:42 +0100

    idk

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -1,18 +1,2 @@
----@diagnostic disable
 local xplr = xplr -- The globally exposed configuration to be overridden.
----@diagnostic enable
-
--- This is the built-in configuration file that gets loaded and sets the
--- default values when xplr loads, before loading any other custom
--- configuration file.
---
--- You can use this file as a reference to create a your custom config file.
---
--- To create a custom configuration file, you need to define the script version
--- for compatibility checks.
---
--- See https://xplr.dev/en/upgrade-guide
---
--- ```lua
--- version = "0.0.0"
--- ```
+version = "0.21.10"
